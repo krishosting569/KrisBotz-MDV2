e74b3bb2d260ae4ac76d0d9918afccc0e30c7107
krishosting569 <111237854+krishosting569@users.noreply.github.com>
2022-10-08 11:54:12 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1307,14 +1307,15 @@ export async function participantsUpdate({ id, participants, action }) {
     thumbnail: await(await fetch(action === 'add' ? wel : lea)).buffer(),
     sourceUrl: sgc
      }}
-  })*/'
+  })*/
+  let welcom = 'https://telegra.ph/file/aab124271570c51f76aac.jpg'
 
   let godbye = 'https://telegra.ph/file/deaf59bc3e5216eaae814.jpg'
   conn.sendButtonImg(id, await(await fetch(action === 'add' ? wel : lea)).buffer(), 'Group Messege', text, action == 'add' ? 'ᴡᴇʟᴄᴏᴍᴇ' : 'sᴀʏᴏɴᴀʀᴀᴀ', action === 'add' ? '.intro' : 'WH MODS BOT', fakes, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: '',
+    mediaUrl: 'https://kris.com',
     mediaType: 2, 
     description: sgc,
-    title: "",
+    title: "Jᴏɪɴ Sɪɴɪ Cᴜʏ",
     body: wm,
     thumbnail: await(await fetch(action === 'add' ? welcom : godbye)).buffer(),
     sourceUrl: sig
