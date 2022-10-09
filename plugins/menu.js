1d00c000c5cc3fcb9f7bef1d69eeb40c26e01374
krishosting569 <111237854+krishosting569@users.noreply.github.com>
2022-10-10 06:07:58 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -329,24 +329,52 @@ let usrs = db.data.users[m.sender]
 ]
 
 let tek = `✧────···[ Dashboard ]···────✧
-*Haii Selamat ${ucapan()} ${conn.getName(m.sender)}*
-
+*${ucapan()} ${conn.getName(m.sender)}*
 ╭━━━━━━━━━━━━━━━━┈─✧
-├❖ ${emot} *ɴᴀᴍᴇ:* ${usrs.registered ? usrs.name : conn.getName(m.sender)}
-├❖ ${emot} *ᴛᴀɢs:* @${m.sender.split`@`[0]}
-├❖ ${emot} *sᴛᴀᴛᴜs:* ${m.sender.split`@`[0] == nomorown ? 'Developer' : (usrs.premiumTime >= 1 ? 'Premium User' : 'Free User')}
-├━━━━━━━━━━━━━━━━┈─⋆
-│  「 *I n f o   B o t 比* 」
-├❖ ${emot} Aktif selama ${mpt}
-├❖ ${emot} Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
-├❖ ${emot} Prefix : [ ${_p} ]
-├❖ ${emot} *${Object.keys(global.db.data.users).length}* Pengguna
-├❖ ${emot} *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
-├❖ ${emot} *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
+┴
+┬
+│${emot} 「 Hai Kak👋 」
+├❖ 「 ${conn.getName(m.sender)} 」
+├❖  Bagaimana Harimu? 😄
+├❖  Terima Kasih Telah Menggunakan Bot Kami
 │
 ├━━━━━━━━━━━━━━━━┈─⋆
+│  「 *U s e r  I n f o 克* 」
+│${emot} *ɴᴀᴍᴇ:* ${usrs.registered ? usrs.name : conn.getName(m.sender)}
+│${emot} *ᴛᴀɢs:* @${m.sender.split`@`[0]}
+│${emot} *sᴛᴀᴛᴜs:* ${m.sender.split`@`[0] == nomorown ? 'Developer' : (usrs.premiumTime >= 1 ? 'Premium User' : 'Free User')}
+│${emot} *ᴘʀᴇᴍɪᴜᴍ:* ${usrs.premiumTime > 1 ? 'Yes': 'No'}
+│
+├━━━━━━━━━━━━━━━━┈─⋆
+│  「 *S t a t u s  I n f o 比* 」
+│${emot} *ᴛɪᴍᴇ:* ${moment.tz('Asia/Jakarta').format('HH')} H  ${moment.tz('Asia/Jakarta').format('mm')} M  ${moment.tz('Asia/Jakarta').format('ss')} S
+│${emot} *ᴜsᴇʀs:* ${Object.keys(global.db.data.users).length}
+│${emot} *ʟɪᴍɪᴛ:* ${usrs.limit}
+│${emot} *ʟᴇᴠᴇʟ:* ${usrs.level}
+│
+├━━━━━━━━━━━━━━━━┈─⋆
+│  「 *I n f o   B o t 比* 」
+│${emot} Aktif selama ${mpt}
+│${emot} Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
+│${emot} Prefix : [ ${_p} ]
+│${emot} *${Object.keys(global.db.data.users).length}* Pengguna
+│${emot} *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
+│${emot} *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
+│
+├━━━━━━━━━━━━━━━━┈─⋆
+│
+│ ▸ *Sumber :* YouTube KRIS BOTZ
 │ ▸ *ᴀᴜᴛʜᴏʀ :* ${nameown}
-├━━━━━━━━━━━━━━━━┈─⋆`
+┴ ▸ *ᴏᴡɴᴇʀ :* ${nameown}
+✧
+┬ 📌 𝗣𝗶𝗻𝗻𝗲𝗱 :
+│ ʙᴇʀɪ ᴊᴇᴅᴀ ʏᴀʜ ᴋᴀᴋ ^ω^
+│
+├━━━━━━━━━━━━━━━━┈─⋆
+│${emot} *ʀᴏʟᴇ:* ${usrs.role}${usrs.premiumTime > 1 ? `
+│${emot} *ᴇxᴘɪʀᴇᴅ ᴘʀᴇᴍɪᴜᴍ:*
+│${emot} ${clockStringP(usrs.premiumTime - new Date())}` : ''}
+╰━━━━━━━━━━━━━━━━┈─◂`
 const listMessage = {
   text: tek,
   footer: wm2,
