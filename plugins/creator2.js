7f8a970b8179c032e75443e376d5d81be293db4f
krishosting569 <111237854+krishosting569@users.noreply.github.com>
2022-10-10 20:31:35 +0700

    Create creator2.js

---
+++ plugins/creator2.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let name = await conn.getName(who)
+
+
+if (command == 'whmods2') {
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6285786211623:+62 857-8621-1623\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
+}
+
+ //knp di enc? , biar kgk di rename deksripsi Developer nya, kalau untuk nomor nya bakal otomatis berubah pas kalian ubah di config.js
+}
+handler.help = ['whmods2']
+handler.tags = ['info']
+
+handler.command = /^(whmods1|whmods2|whmods3)$/i
+
+export default handler
